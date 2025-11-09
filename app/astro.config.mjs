7dc3110c71edeed374e99b0c37eb923360ee4f05
Nicolas Decoopman <171740134+NCSdecoopman@users.noreply.github.com>
2025-11-09 10:34:34 +0100

    logo

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://NCSdecoopman.github.io',
-  base: '/niveo',
+  site: 'https://ncsdecoopman.github.io',
+  base: '/niveo/',               // <-- slash final
   server: { port: 4321 },
   vite: {
     build: { sourcemap: false },
